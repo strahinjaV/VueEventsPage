9708e27fc67509b8b9f50eef3ce8d993280260d4
Strahinja <strahinjav98@gmail.com>
2023-02-24 04:21:52 +0100

    first commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Events for {{ user.user.name }}</h2>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event"/>
+    <template v-if="page != 1">
+      <router-link :to="{name: 'event-list', query: {page: page-1 } }" rel="prev">Prev Page |</router-link>
+    </template>
+    <template v-if="hasNextPage">
+      <router-link :to="{name: 'event-list', query: {page: page+1 } }" rel="next">Next Page</router-link>
+    </template>
+    
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    EventCard
+  },
+  created() {
+    this.$store.dispatch('event/fetchEvents', {
+      perPage: 3,
+      page: this.page
+    })
+  },
+  computed:{
+    page(){
+      return parseInt(this.$route.query.page) || 1
+    },
+    hasNextPage(){
+      return this.event.eventsTotal > this.page * 3
+    },
+    ...mapState(['event','user'])
+  } 
+  
+}
+</script>
